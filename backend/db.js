e48d6ecf4829b184c9a18c2c84f10969c07378fa
Thirsanth <thirsanthg@gmail.com>
2024-08-29 11:18:01 +0530

    Added an new account table

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { object, string, number } = require("zod");
 
 mongoose.connect("mongodb+srv://thirsanth:mother123@cluster0.b3q8b7l.mongodb.net/paytm?retryWrites=true&w=majority&appName=Cluster0");
 
@@ -31,7 +32,21 @@ const schema = mongoose.Schema({
     }
 })
 
+const accountsschema = mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    balance:{
+        type:Number,
+        required:true
+    }
+})
+
 const User = mongoose.model("User",schema);
+const Account = mongoose.model("Accounts",accountsschema);
 module.exports={
-    User
+    User,
+    Account
 };
